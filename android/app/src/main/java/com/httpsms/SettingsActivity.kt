ce667a5639686fd6e238fa229e2699544f696b49
DeliChow SMS Gateway Lab <delichow-sms-gateway-lab@example.invalid>
2026-07-08 14:51:20 +0000

    chore: rebrand Android visible labels

--- android/app/src/main/java/com/httpsms/SettingsActivity.kt
+++ android/app/src/main/java/com/httpsms/SettingsActivity.kt
@@ -137,7 +137,7 @@ class SettingsActivity : AppCompatActivity() {
         Timber.d("logout button clicked")
         MaterialAlertDialogBuilder(this)
             .setTitle("Confirm")
-            .setMessage("Are you sure you want to logout of the httpSMS App?")
+            .setMessage("Are you sure you want to logout of the DeliSMS App?")
             .setNeutralButton("Cancel"){ _, _ -> Timber.d("logout dialog canceled") }
             .setPositiveButton("Logout"){_, _ ->
                 Timber.d("logging out user")
--- android/app/src/main/java/com/httpsms/services/StickyNotificationService.kt
+++ android/app/src/main/java/com/httpsms/services/StickyNotificationService.kt
@@ -65,8 +65,8 @@ class StickyNotificationService: Service() {
         )
 
         return builder
-            .setContentTitle("httpSMS Listener")
-            .setContentText("httpSMS is listening for sent and received SMS messages in the background.")
+            .setContentTitle("DeliSMS Listener")
+            .setContentText("DeliSMS is listening for sent and received SMS messages in the background.")
             .setContentIntent(pendingIntent)
             .setOngoing(true)
             .setSmallIcon(R.drawable.ic_stat_name)
